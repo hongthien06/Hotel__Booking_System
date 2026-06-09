f2ba615fa643831e04b59165e9cfaa7699f8d870
nguyenan12 <nguyenann1204@gmail.com>
2026-06-09 21:05:31 +0700

    feat: initialize SQL Server database schema with hotel booking management structures

--- database/HotelBookingDB.sql
+++ database/HotelBookingDB.sql
@@ -799,6 +799,8 @@ BEGIN
 END
 GO
 
+ALTER TABLE Payments ADD language VARCHAR(10) NULL DEFAULT 'vi';
+
 -- Seed data for membership_tiers
 INSERT INTO membership_tiers (tier_code, tier_level, discount_pct, min_total_spent, min_booking_count,
                                display_name_vi, display_name_en, color_code, benefits_vi, benefits_en)
